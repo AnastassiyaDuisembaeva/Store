55be3a19cef0561adeebc8afc09c8e1793054c96
Anastassiya Duisembayeva <imperator-taya@mail.ru>
2019-06-19 11:36:37 +0600

    Add toolbar whit too items: search and login

--- app/src/main/java/com/example/bsfragments/MainActivity.java
+++ app/src/main/java/com/example/bsfragments/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.bsfragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener  {
-
+    private FirebaseAuth  mAuth;
+    private Toolbar toolbar;
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -33,9 +39,28 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        MenuItem accaunt = (MenuItem) findViewById(R.id.navigation_accaunt);
+        if (user != null) {
+            accaunt.setVisible(true);
+
+        } else {
+            accaunt.setVisible(false);
+
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         //loading the default fragment
         loadFragment(new books());
@@ -44,6 +69,25 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         BottomNavigationView navigation = findViewById(R.id.nav_view);
         navigation.setOnNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_search) {
+            Intent intent = new Intent(this, SearchActivity.class);
+            startActivity(intent);
+        }
+        if(item.getItemId() == R.id.action_login) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+        return true;
+    }
+
     private boolean loadFragment(Fragment fragment) {
         //switching fragment
         if (fragment != null) {
